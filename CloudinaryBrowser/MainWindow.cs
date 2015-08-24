5e1d3e1e7875aee728646509d8b802f1cc4a56e7
Adam Van Ornum <adam@14four.com>
2015-08-24 09:55:44 -0700

    Initial work on GUI...learning GTK#

---
+++ CloudinaryBrowser/MainWindow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void LoginClicked (object sender, EventArgs e)
+	{
+		var label = new Label ("Login pressed!") { Visible = true };
+		InfoListContainer.Attach (label, 0, 1, InfoListContainer.NRows, InfoListContainer.NRows + 1);
+	}
+}
